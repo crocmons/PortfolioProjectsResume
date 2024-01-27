f233a01cdbe35f0f0bd3bd0ad9619e7f52a6bb3c
Your Name <you@example.com>
2024-01-27 10:41:25 +0600

    added new project

--- components/errorNoHomePage.tsx
+++ components/errorNoHomePage.tsx
@@ -1,7 +1,7 @@
 const ErrorNoHomePage = () => {
   return (
     <div className="max-w-5xl mx-auto px-6 py-20 text-gray-600">
-      <h1 className="text-2xl text-red-600 font-semibold mb-8">
+      <h1 className="text-4xl text-red-600 font-semibold mb-8">
         Warning: there is no page with a "home" slug.
       </h1>
       <p className="mb-6">
